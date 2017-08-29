1f85ea8b6342fc897aa691fa9a3cbe1ce9e7c0d1
Evarist <evaristjaumet@gmail.com>
2017-08-29 20:52:28 +0200

    faltan test de integracion

--- src/test/java/com/everis/alicante/courses/beca/summer17/friendsnet/controller/PersonControllerIT.java
+++ src/test/java/com/everis/alicante/courses/beca/summer17/friendsnet/controller/PersonControllerIT.java
@@ -3,6 +3,9 @@ package com.everis.alicante.courses.beca.summer17.friendsnet.controller;
 import com.everis.alicante.courses.beca.summer17.friendsnet.dao.interfaces.PersonDAO;
 import com.everis.alicante.courses.beca.summer17.friendsnet.entity.Person;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,9 +20,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -45,32 +45,26 @@ public class PersonControllerIT {
     }
 
 
+//    @Test
+//    public void testFindAllNoContent() throws JSONException {
+//        //Arrange
+//        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+//
+//        // Act
+//        ResponseEntity<String> response = restTemplate.exchange(
+//                createURLWithPort("/person"),
+//                HttpMethod.GET, null, String.class);
+//
+//        // Assert
+//        JSONAssert.assertEquals("[]", response.getBody(), false);
+//    }
+
     @Test
-    public void testFindAllNoContent() throws JSONException {
-        //Arrange
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-
-        // Act
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/person"),
-                HttpMethod.GET, null, String.class);
-
-        // Assert
-        JSONAssert.assertEquals("[]", response.getBody(), false);
-    }
-
-    @Test
+    @DatabaseSetup("/initial-person.xml")
+    @ExpectedDatabase(value = "/initial-person.xml", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED)
     public void testFindAllWithContent() throws JSONException {
         //Arrange
         HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        Person person =new Person();
-        person.setName("Evarist");
-        person.setSurname("Jaume");
-        dao.save(person);
-        Person p2 = new Person();
-        p2.setName("pepe");
-        p2.setSurname("paca");
-        dao.save(p2);
 
         // Act
         ResponseEntity<String> response = restTemplate.exchange(
@@ -78,18 +72,37 @@ public class PersonControllerIT {
                 HttpMethod.GET, null, String.class);
 
         // Assert
-        JSONAssert.assertEquals("[{'id': 1, 'name':'Evarist', 'surname':'Jaume'},{'id': 2, 'name':'pepe', 'surname':'paca'}]", response.getBody(), false);
+        JSONAssert.assertEquals("[{'id': 1, 'name':'Evarist', 'surname':'Jaume'}]", response.getBody(), false);
     }
 
-/*    @Test
-    public void testFindAll() throws JSONException{
+    @Test
+    public void testGetById() throws JSONException {
+        //Arrange
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
         //Act
-        List<Person> all = (List<Person>)dao.findAll();
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/person/1"),
+                HttpMethod.GET, null, String.class);
 
         //Assert
-//        JSONAssert.assertEquals(2, all.size());
+        JSONAssert.assertEquals("{'id': 1, 'name':'Evarist', 'surname':'Jaume'}", response.getBody(), false);
 
-    }*/
+    }
+
+//    @Test
+//    public void testRemove() throws JSONException {
+//        //Arrange
+//        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+//
+//        //Act
+//        ResponseEntity<String> response = restTemplate.exchange(
+//                createURLWithPort("/person/1"),
+//                HttpMethod.DELETE, null, String.class);
+//        //Assert
+//        JSONAssert.assertEquals(null, response.getBody(), false);
+//
+//    }
 
     private String createURLWithPort(String uri) {
         return "http://localhost:" + port + uri;
